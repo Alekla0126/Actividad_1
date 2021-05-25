709d29a06e355b6e44cf8e59136fd5322403f2e1
alekla0126 <alekla0126@gmail.com>
2021-05-25 18:52:15 -0500

    V1.2

---
+++ Actividad_1/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<script src="https://apis.google.com/js/platform.js" async defer></script>
+		<script language="JavaScript" type="text/javascript" src="/google/getProfile.js"></script>
+	</head>
+	<meta name="google-signin-client_id" content="169591568983-p4fbahaeeottl0ijsa4ggva8ogu6hgjj.apps.googleusercontent.com">
+	<head>
+		<div class="g-signin2" data-onsuccess="onSignIn"></div>
+	</head>
+	<body></body>
+</html>
