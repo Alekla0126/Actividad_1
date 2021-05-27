709c052070873769a8f67c49bb27945ddcf977ef
alekla0126 <alekla0126@gmail.com>
2021-05-26 21:37:12 -0500

    Llamada al backend V1.27

--- google/getProfile.js
+++ google/getProfile.js
@@ -2,7 +2,7 @@ function getUser(googleUser)
 {
     var id_token = googleUser.getAuthResponse().id_token;
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://formate.app/api/tokensignin.php');
+    xhr.open('POST', 'https://alekla.com/homeoffix/api/tokensignin.php');
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onload = function ()
     {
