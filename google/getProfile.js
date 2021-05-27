b29fb7ccd36478fc5fec4b76f2d0907d977fd64d
alekla0126 <alekla0126@gmail.com>
2021-05-26 19:50:27 -0500

    Llamada al backend V1.8

--- google/getProfile.js
+++ google/getProfile.js
@@ -8,14 +8,17 @@ function getUser(googleUser)
     {
         console.log('Signed in as: ' + xhr.responseText);
     };
-    xhr.send('idtoken=' + id_token);
+    xhr.send('idtoken = ' + id_token);
 }
 
 function onSignIn(googleUser)
 {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // Do not send to your backend! Use an ID token instead.
+    console.log('ID: ' + profile.getId());
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    // This is null if the 'email' scope is not present.
+    console.log('Email: ' + profile.getEmail());
+    getUser(googleUser);
 }
