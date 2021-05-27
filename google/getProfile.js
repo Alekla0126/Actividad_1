38ac954c501345d9638f5a3ff7a96e4ef5fd75bd
alekla0126 <alekla0126@gmail.com>
2021-05-26 19:32:11 -0500

    Llamada al backend V1.3

--- google/getProfile.js
+++ google/getProfile.js
@@ -1,20 +1,21 @@
-function onSignIn(googleUser) 
+function onSignIn(googleUser)
 {
-	var id_token = googleUser.getAuthResponse().id_token;
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', 'https://alekla.com/homeoffix/api/verify');
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.onload = function() {
-	console.log('Signed in as: ' + xhr.responseText);
-	};
-	xhr.send('idtoken=' + id_token);
+    var id_token = googleUser.getAuthResponse().id_token;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://alekla.com/homeoffix/api/verify');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function ()
+    {
+        console.log('Signed in as: ' + xhr.responseText);
+    };
+    xhr.send('idtoken=' + id_token);
 }
 
 function getInfo(googleUser)
 {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
