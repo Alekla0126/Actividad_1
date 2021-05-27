c1c5aff2bc95c2754ba805d1600758a85acef600
alekla0126 <alekla0126@gmail.com>
2021-05-26 20:32:55 -0500

    Llamada al backend V1.11

--- api/tokensignin.php
+++ api/tokensignin.php
@@ -3,18 +3,22 @@
 	require_once 'vendor/autoload.php';
 
 	// Get $id_token via HTTPS POST.
-	$id_token= $_POST['id_token'];
-	$CLIENT_ID = "559809341234-elmqbps1vrlkuirmv2muvcsl51vigvf4.apps.googleusercontent.com";
+	$id_token = $_POST['id_token'];
+	$CLIENT_ID = '559809341234-elmqbps1vrlkuirmv2muvcsl51vigvf4.apps.googleusercontent.com';
 	// Specify the CLIENT_ID of the app that accesses the backend
 	$client = new Google_Client(['client_id' => $CLIENT_ID]);
-	$payload = $client->verifyIdToken($id_token);
-	if ($payload)
+	$payload = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='.$id_token;
+	$json = file_get_contents($payload);
+	$userInfoArray = json_decode($json, TRUE);
+	$googleEmail = $userInfoArray['email'];
+	$google_id = $userInfoArray['sub'];
+
+	if ($googleEmail)
 	{
-		$userid = $payload['sub'];
-		// If request specified a G Suite domain:
-		//$domain = $payload['hd'];
+		echo "validated".$googleEmail;
 	}
 	else
 	{
-		// Invalid ID token
+		echo "inValidate";
 	}
+
